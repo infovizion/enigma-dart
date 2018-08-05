74acbcc95cf3e94a3e5ce8dd984cdbcbfc268afe
Vadim Tsushko <vadimtsushko@gmail.com>
2018-08-05 15:37:38 +0500

    SenseEnigma added (Certificate based authentication for QlikSense Enterpise)

--- enigma/example/sense/get_apps.dart
+++ enigma/example/sense/get_apps.dart
@@ -5,26 +5,10 @@ import 'connection_info.dart';
 const APP_ID = '3261f1ef-3b98-460a-bd4a-3bae0e46cef0';
 
 main() async {
-  await _testQES();
-}
-
-_testQES() async {
   var enigma = new SenseEnigma(host, port, userDir, userId);
   await enigma.initSession();
   var global = enigma.open();
-  var doc = await global.openDoc(APP_ID);
-  var properties = new GenericObjectProperties((b) => b
-    ..info.type = 'my-straight-hypercube'
-    ..hyperCubeDef
-        .dimensions
-        .add(new NxDimension((b) => b..def.fieldDefs.add('Год')))
-    ..hyperCubeDef.measures.add(new NxMeasure((b) => b..def.def = 'Sum(Сумма)'))
-    ..hyperCubeDef.initialDataFetch.add(new NxPage((b) => b
-      ..height = 5
-      ..width = 2)));
-
-  var cube = await doc.createSessionObject(properties);
-  var layout = await cube.getLayout();
-  print('Hypercube data pages: ${layout.hyperCube.dataPages}');
+  var apps = await global.getDocList();
+  print('Application list: $apps');
   await enigma.close();
 }
